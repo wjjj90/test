f3e52495675d6cc38edb70931cba7bb05f33b0f4
YWJ <w960133@gmail.com>
2022-12-08 16:23:52 +0800

    1208

---
+++ serial_test_PC.py
@@ -0,0 +1,10 @@
+import serial
+serBarCode = serial.Serial('COM19', 115200, timeout=1)
+while True:
+
+    #read data from serial port
+    data = serBarCode.readline()
+
+    #if there is smth do smth
+    if len(data) >= 1:
+        print(data.decode("utf-8"))
